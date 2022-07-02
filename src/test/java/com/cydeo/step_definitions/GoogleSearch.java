90fc65a25399150b402223d8b66502ec56fc2a3e
zivadinjo <zivadinjo1985@gmail.com>
2022-07-02 12:10:12 -0500

    Cucumber day2

---
+++ src/test/java/com/cydeo/step_definitions/GoogleSearch.java
@@ -0,0 +1,29 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.GoogleSearchPage;
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Keys;
+import org.junit.Assert;
+
+public class GoogleSearch {
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @Given("User is on Google search page")
+    public void user_is_on_google_search_page() {
+        Driver.getDriver().get("https://www.google.com/");
+    }
+    @When("User types apple in the google search box and click enter")
+    public void user_types_apple_in_the_google_search_box_and_click_enter() {
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+    }
+    @Then("User sees apple - Google Search is in the google title")
+    public void user_sees_apple_google_search_is_in_the_google_title() {
+        Assert.assertEquals("Title verification failed!","apple - Google Search",Driver.getDriver().getTitle());
+        Driver.closeDriver();
+    }
+
+}
---
+++ src/test/resources/features/google_search.feature
@@ -0,0 +1,9 @@
+Feature: Google Search Functionality Title Verification
+  User story: As a user, when I'm on the Google search page
+  I should be able to search whatever I want,an see relevant information
+
+  @wip
+  Scenario: Search functionality result title verification
+    Given User is on Google search page
+    When User types apple in the google search box and click enter
+    Then User sees apple - Google Search is in the google title
