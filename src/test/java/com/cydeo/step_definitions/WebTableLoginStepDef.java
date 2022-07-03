3a89760f2736125d1d24fddf3ad2487522cd77a1
zivadinjo <zivadinjo1985@gmail.com>
2022-07-03 15:43:49 -0500

    Cucumber day3

--- src/test/java/com/cydeo/step_definitions/WebTableLoginStepDef.java
+++ src/test/java/com/cydeo/step_definitions/WebTableLoginStepDef.java
@@ -3,14 +3,34 @@ package com.cydeo.step_definitions;
 import com.cydeo.pages.WebTableLoginPage;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.it.Ma;
 import org.junit.Assert;
+
+import java.util.Map;
 
 public class WebTableLoginStepDef {
 
     WebTableLoginPage webTableLoginPage = new WebTableLoginPage();
+
+    @When("User enters below credentials")
+    public void user_enters_below_credentials(Map<String,String> credentials) {
+
+        webTableLoginPage.login(credentials.get("username"),credentials.get("password"));
+
+    }
+
+
+    @When("User enters username {string} and password {string} and login")
+    public void user_enters_username_and_password_and_login(String username, String password) {
+        webTableLoginPage.login("Test","Tester");
+    }
+
+
+
 
     @Given("User is on web table login page")
     public void user_is_on_web_table_login_page() {
@@ -30,10 +50,15 @@ public class WebTableLoginStepDef {
         webTableLoginPage.inputPassword.sendKeys(password);
 
     }
-    @Then("User should see url contains orders")
-    public void user_should_see_url_contains_orders() {
+
+    @And("User click login button")
+    public void userClickLoginButton() {
 
         webTableLoginPage.loginBtn.click();
+
+    }
+    @Then("User should see url contains orders")
+    public void user_should_see_url_contains_orders() {
 
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("orders"));
 
--- src/test/resources/features/webAppLogin.feature
+++ src/test/resources/features/webAppLogin.feature
@@ -1,9 +1,22 @@
-
 Feature: User should be able to login with correct credentials
 
-  @wip @webAppLogin
-  Scenario:
+  Background:
     Given User is on web table login page
+
+  @webAppLogin
+  Scenario:
     When User enters username "Test"
     And User enters password "Tester"
+    And User click login button
     Then User should see url contains orders
+
+
+  Scenario: positive login scenario
+    When User enters username "Test" and password "Tester" and login
+    Then User should see url contains orders
+
+  @wip
+  Scenario:
+    When User enters below credentials
+      | username | Test   |
+      | password | Tester |
