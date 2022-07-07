448d922df197ecdb5b1b3618d79e6f3c150e4125
zivadinjo <zivadinjo1985@gmail.com>
2022-07-07 16:25:11 -0500

    Cucumber day5

--- src/test/java/com/cydeo/runner/Runner.java
+++ src/test/java/com/cydeo/runner/Runner.java
@@ -7,11 +7,14 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt"
+        },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@wip"  //it will run only tags that are included here ex. "@wip"  , ex."@wip or @smoke" , ex."@wip and @smoke" , ex. "@wip and not @smoke"
+        tags = "@googleSearch"  //it will run only tags that are included here ex. "@wip"  , ex."@wip or @smoke" , ex."@wip and @smoke" , ex. "@wip and not @smoke"
 )
 
 public class Runner {
